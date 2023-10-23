596afec3b75d6622daf3ebd880e161f4dee28ef0
Jisha Joshi <jisha.joshi@freedom-finance.com>
2023-10-23 16:44:08 +0100

    feat: removed unused css

--- src/App.vue
+++ src/App.vue
@@ -30,18 +30,6 @@ export default {
   box-sizing: border-box;
   font-family: 'Fira sans', sans-serif;
 }
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 body {
   margin:0;
   background: #eee;
